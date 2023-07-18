2c74fb80968a589ab659b87155886c196c731b6f
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2023-07-18 10:30:23 +0530

    adding css for styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './userlist.css';
 
 function UserList() {
   const [users, setUsers] = useState([]);
@@ -17,25 +18,35 @@ function UserList() {
 
   return (
     <div>
-      <h1>User Data</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.username}</td>
+      <header className="header">
+        <img src="logo.png" alt="Logo" className="logo" />
+      </header>
+
+      <div className="container">
+        <h1>Final Assessment Test</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Username</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.username}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <footer className="footer">
+        <p>&copy; 2023 Final Assessment Test.. All rights reserved. Task for Hitbullseye</p>
+      </footer>
     </div>
   );
 }
